c922dddd7e55dc05c7b3c6b19f7e9c36b35b28b6
Raw-Coder-Rahul <rb3335167@gmail.com>
2025-12-22 01:24:07 +0530

    Deployment

--- health_diet/vite.config.js
+++ health_diet/vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Increase the default 500 KB warning limit
+    chunkSizeWarningLimit: 1000, // in KB
+
+    // Optional: split vendor libraries into separate chunks
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          redux: ['react-redux', '@reduxjs/toolkit'],
+          mui: ['@mui/material', '@mui/icons-material'],
+        },
+      },
+    },
+  },
 })
