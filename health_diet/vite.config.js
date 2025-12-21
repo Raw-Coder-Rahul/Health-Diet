40af34a81f664ae189755f4f9dd11cf685dbc187
Raw-Coder-Rahul <rb3335167@gmail.com>
2025-12-22 02:09:01 +0530

    Deployment

--- health_diet/vite.config.js
+++ health_diet/vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+// https://vite.dev/config/ 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    // Raise the warning threshold (default is 500 KB)
-    chunkSizeWarningLimit: 1000, // in KB
-  },
+   plugins: [react()], 
 })
